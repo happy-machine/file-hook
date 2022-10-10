5d6e500d43d02aa751464bcb1e04b8c8ca0dc116
Dan Stein <dan.stein@generalbioinformatics.com>
2022-10-10 14:01:43 +0100

    chore(all): changes to file structure

--- src/config/constants.rs
+++ src/config/constants.rs
@@ -9,11 +9,9 @@ pub struct Env {
 pub fn get_env() -> Env {
     return Env {
         recursive_mode: env::var("RECURSIVE_MODE").unwrap_or("false".to_string()).parse().unwrap(),
-        request_sensor_path: env::var("REQUEST_SENSOR_PATH").unwrap_or("./".to_string()).parse().unwrap(),
+        request_sensor_path: env::var("REQUEST_SENSOR_PATH").unwrap_or("./".to_string()),
         endpoint: env::var("ENDPOINT")
             .expect("\nPlease set ENDPOINT environment variable to the endpoint you want to recieve file and folder names.\nTo set, execute \'export ENDPOINT=\"myapi.com/ExamplefileSensorEndpoint\"\'\n")
-            .parse()
-            .unwrap()
     };
 }
 
