ea2acbd18d6744b37577732f7d7a92bf472b0963
Dan Stein <dan.stein@generalbioinformatics.com>
2022-10-19 19:25:17 +0100

    in progress

--- src/config/constants.rs
+++ src/config/constants.rs
@@ -1,22 +1,56 @@
+use std::collections::HashMap;
 use std::env;
+use std::any::Any;
 
-pub struct Env {
-    pub recursive_mode: bool,
-    pub request_sensor_path: String,
-    pub endpoint: String
+enum EnvTypeKind {
+    BOOL,
+    STR,
+    I8,
+    I16,
+    I32,
+    I64,
+    U8,
+    U16,
+    U32,
+    U64,
+    F32,
+    F64
 }
 
-pub fn get_env() -> Env {
-    struct unit;
-    unit["test"]="yo";
-    return Env {
-        recursive_mode: env::var("RECURSIVE_MODE").unwrap_or("false".to_string()).parse().unwrap(),
-        request_sensor_path: env::var("REQUEST_SENSOR_PATH").unwrap_or("./".to_string()),
-        endpoint: env::var("ENDPOINT")
-            .expect("\nPlease set ENDPOINT environment variable to the endpoint you want to recieve file and folder names.\nTo set, execute \'export ENDPOINT=\"myapi.com/ExamplefileSensorEndpoint\"\'\n")
-    };
+
+pub fn safe_env(key: String) -> Any {
+    env::var("ENDPOINT")
+        .expect("\nPlease set ENDPOINT environment variable to the endpoint you want to recieve file and folder names.\nTo set, execute \'export ENDPOINT=\"myapi.com/ExamplefileSensorEndpoint\"\'\n");
+    let this_env: HashMap<&str, (EnvTypeKind, String)> = HashMap::from([
+        ("RECURSIVE_MODE", (EnvTypeKind::BOOL, "false".to_string())),
+        ("REQUEST_SENSOR_PATH", (EnvTypeKind::STR, "".to_string())),
+        ("ENDPOINT", (EnvTypeKind::STR, "./".to_string()))
+    ]);
+    for (key_inner, val) in env::vars() {
+        if this_env.contains_key(&key as &str){
+            match this_env.get(&key as &str){
+                //BOOL => return val.parse().unwrap_or((this_env.get(&key as &str)).1)
+               BOOL => println!("boom"),
+            }
+            return val;
+        } else if key == key_inner {
+            return val;
+        }
+        println!("env var: {:?}", key)
+    }
+    return "random".to_string();
+    // println!("test")
 }
 
+// env::var("ENDPOINT").expect("\nPlease set ENDPOINT environment variable to the endpoint you want to recieve file and folder names.\nTo set, execute \'export ENDPOINT=\"myapi.com/ExamplefileSensorEndpoint\"\'\n");
+// let RECURSIVE_MODE =
+//     env::var("RECURSIVE_MODE").unwrap_or("false".to_string())
+    
+pub struct Env2 {
+    recursive_mode: bool,
+    request_sensor_path: String,
+    endpoint: String
+}
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,38 @@
-use notify::{
-  event::{CreateKind, EventKind},
-  RecommendedWatcher, 
-  RecursiveMode, 
-  Watcher, 
-  Config
-};
+use std::any::Any;
+// use notify::{
+//   event::{CreateKind, EventKind},
+//   RecommendedWatcher, 
+//   RecursiveMode, 
+//   Watcher, 
+//   Config
+// };
 
-use std::error::Error;
-use std::collections::HashMap;
-use std::{path::Path, path::PathBuf};
+// use std::error::Error;
+// use std::collections::HashMap;
+// use std::{path::Path, path::PathBuf};
 
 mod config;
 
-fn post(full_path:String, type_string:String) -> Result<(), Box<dyn Error>> {
-  let env = config::constants::get_env();
-  let path = PathBuf::from(full_path);
-  let target = path.file_name().unwrap();
-  let mut map = HashMap::new();
-  map.insert("path", target.to_str().unwrap());
-  map.insert("type", &type_string);
-  let client = reqwest::blocking::Client::new();
-  let _res = client.post(env.endpoint)
-      .json(&map)
-      .send();
-  Ok(())
-}
+// fn post(full_path:String, type_string:String) -> Result<(), Box<dyn Error>> {
+//   let env = config::constants::get_env();
+//   let path = PathBuf::from(full_path);
+//   let target = path.file_name().unwrap();
+//   let mut map = HashMap::new();
+//   map.insert("path", target.to_str().unwrap());
+//   map.insert("type", &type_string);
+//   let client = reqwest::blocking::Client::new();
+//   let _res = client.post(env.endpoint)
+//       .json(&map)
+//       .send();
+//   Ok(())
+// }
 
 fn main() {
-  let env = config::constants::get_env();
-  if let Err(e) = watch(env.request_sensor_path) {
-      println!("error: {:?}", e)
-  }
+  let test: dyn Any = config::constants::safe_env("RECURSIVE_MODE".to_string());
+  //println!("test {:?}", test.1)
+  // let env = config::constants::get_env();
+  // if let Err(e) = watch(env.request_sensor_path) {
+  //     println!("error: {:?}", e)
+  // }
 }
 
-fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
-    let env = config::constants::get_env();
-    let (tx, rx) = std::sync::mpsc::channel();
-    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
-    if env.recursive_mode {
-      watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
-    } else {
-      watcher.watch(path.as_ref(), RecursiveMode::NonRecursive)?;
-    }
-    let mut folder_history = vec![];
-
-    for res in rx {
-      if folder_history.len() > 50 {
-        let folder_history_safe_length = folder_history.len().saturating_sub(50);
-        folder_history.truncate(folder_history_safe_length);
-      }
-      match res {
-        Ok(event) => {
-          match event.kind {
-            EventKind::Create(CreateKind::File) => {
-              println!("new file: {} ", event.paths[0].display().to_string());   
-              post(event.paths[0].display().to_string(), "file".to_string()); 
-            }
-            EventKind::Create(CreateKind::Folder) => {
-              if !folder_history.contains(&event.paths[0].display().to_string()){
-                println!("new folder: {} ", event.paths[0].display().to_string());  
-                post(event.paths[0].display().to_string(), "folder".to_string());
-                folder_history.push(event.paths[0].display().to_string());
-              }
-            }
-            _ => { /* something else changed */ }
-          }
-        }
-        Err(e) => {
-          panic!("watch error: {:?}", e);
-        }
-      }
-    }
-  Ok(())
-}
